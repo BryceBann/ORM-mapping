428e675cfb73a4b3812111224cc6a367c6d21349
BryceBann <109399715+BryceBann@users.noreply.github.com>
2022-10-27 15:51:47 -0500

    not much updated mapping of code and study on orm

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,12 +7,14 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll()
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  // Product.findAll or findOne
 });
 
 // create new product
@@ -91,6 +93,7 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+   Product.destroy() //needs a where to delete??
 });
 
 module.exports = router;
