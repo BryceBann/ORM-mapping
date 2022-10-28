8b78b08e991bb2baefdec14f754a417cb7672662
BryceBann <109399715+BryceBann@users.noreply.github.com>
2022-10-28 14:13:04 -0500

    worked on tag page get error

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,28 +1,34 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
+const { findByPk } = require('../../models/Product');
 
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll()
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  // Category.findOne or findByPk
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create()
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update()
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy()
 });
 
 module.exports = router;
